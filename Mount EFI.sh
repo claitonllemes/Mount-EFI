1105b9403360807e86e276caf341722de2e1844b
Claiton Lemes <99222756+claitonllemes@users.noreply.github.com>
2024-06-08 14:21:11 -0300

    Update Mount EFI.sh
    Update to version 0.2.0

--- Mount EFI.sh
+++ Mount EFI.sh
@@ -17,26 +17,41 @@ GUID_DISKS=$(diskutil list | grep 'GUID_partition_scheme' | awk '{print $NF}')
 # Flag para verificar se a partição EFI foi montada
 EFI_MOUNTED=false
 
-# Verifica cada disco GPT para encontrar e montar a partição EFI
+# Verifica cada disco GPT para encontrar a partição EFI
 for disk in $GUID_DISKS; do
-    # Alterado para corresponder exatamente com a partição chamada 'EFI'
     EFI_PARTITION=$(diskutil list "$disk" | awk '/ EFI / {print $NF}' | head -n 1)
     if [ -n "$EFI_PARTITION" ]; then
-        if sudo diskutil mount "$EFI_PARTITION"; then
+        # Verifica se a partição EFI está montada
+        MOUNT_POINT=$(diskutil info "$EFI_PARTITION" | grep 'Mount Point' | awk -F: '{print $2}' | xargs)
+        if [ -n "$MOUNT_POINT" ]; then
             EFI_MOUNTED=true
             break
         fi
     fi
 done
 
-# Verifica se a partição EFI foi montada com sucesso
+# Se a partição EFI estiver montada, desmonta-a
 if [ "$EFI_MOUNTED" = true ]; then
-    echo "✅ EFI montada com sucesso"
+    if sudo diskutil unmount "$EFI_PARTITION"; then
+        echo "🔴 EFI desmontada com sucesso"
+    else
+        print_error "❌ Falha ao desmontar a partição EFI."
+    fi
 else
-    print_error "❌ Nenhuma partição EFI encontrada."
+    # Se a partição EFI não estiver montada, tenta montá-la
+    for disk in $GUID_DISKS; do
+        EFI_PARTITION=$(diskutil list "$disk" | awk '/ EFI / {print $NF}' | head -n 1)
+        if [ -n "$EFI_PARTITION" ]; then
+            if sudo diskutil mount "$EFI_PARTITION"; then
+                echo "🟢 EFI montada com sucesso"
+                EFI_MOUNTED=true
+                break
+            fi
+        fi
+    done
+
+    # Verifica se a partição EFI foi montada com sucesso
+    if [ "$EFI_MOUNTED" = false ]; then
+        print_error "❌ Nenhuma partição EFI encontrada."
+    fi
 fi
-
-# Exibe os discos na area de trabalho
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-
-killall Finder
